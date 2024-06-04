5297f860dd835825f166aed5f298afc19633832b
ACE30801 <elshaer118@gmail.com>
2024-06-04 13:18:45 +0300

    cleaning main

---
+++ Core/Inc/APP_config.h
@@ -0,0 +1,22 @@
+/*
+ * APP_config.h
+ *
+ *  Created on: Jun 4, 2024
+ *      Author: Administrator
+ */
+
+#ifndef INC_APP_CONFIG_H_
+#define INC_APP_CONFIG_H_
+
+/*
+ * Automatic_ON
+ * Automatic_OFF
+ */
+#define speed_control_outomatic         Automatic_ON
+#define V2V_OPTION                      V2V_ENABLE
+
+#define SO_SHORT_DISTANCE  10.0
+#define UN_SAFE_DISTANCE   20.0
+
+
+#endif /* INC_APP_CONFIG_H_ */
